f65c83791c575d241bccbe3cb78b3f5c16c96078
liu.xuandi <lxd19850415@aliyun.com>
2015-01-11 18:50:41 +0800

    数据库表结构更新

--- sqlData/backup.sql
+++ sqlData/backup.sql
@@ -4,7 +4,7 @@ Source Host: localhost
 Source Database: blog
 Target Host: localhost
 Target Database: blog
-Date: 2015/1/8 23:29:24
+Date: 2015/1/11 18:49:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -13,7 +13,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article` (
-  `id` tinyint(4) NOT NULL,
+  `id` tinyint(4) NOT NULL auto_increment,
   `title` varchar(256) NOT NULL,
   `content` longtext NOT NULL,
   `author` varchar(256) NOT NULL,
@@ -31,10 +31,6 @@ CREATE TABLE `user` (
   `id` tinyint(4) NOT NULL auto_increment,
   `account` char(32) NOT NULL,
   `password` char(32) NOT NULL,
+  `email` char(32) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records 
--- ----------------------------
-INSERT INTO `article` VALUES ('1', '计算机技术', '计算机技术的内容非常广泛，可粗分为计算机系统技术、计算机器件技术、计算机部件技术和计算机组装技术等几个方面。计算机技术包括：运算方法的基本原理与运算器设计、指令系统、中央处理器(CPU)设计、流水线原理及其在CPU设计中的应用、存储体系、总线与输入输出。', 'liuxuandi', '2015-01-06 23:29:43', '2015-01-06 23:29:48', '');
