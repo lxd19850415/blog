bcbdd369b933486494b252b790bd24613dfb1384
xuandi.liu <xuandi.liu@gumichina.com>
2015-02-09 13:58:07 +0800

    在mac下面运行，发现路径有些问题

--- config.php
+++ config.php
@@ -10,15 +10,15 @@ $config=array(
         'left_delimiter'=>'{',
         'right_delimiter'=>'}',
         'template_dir'=>'tpl',
-        'compile_dir'=>'data\template_c'
+        'compile_dir'=>'data/template_c'
     ),
-'dbconfig'=>array(
-    'dbhost'=>'127.0.0.1',
-    'dbuser'=>'root',
-    'dbpsw'=>'root',
-    'dbname'=>'blog',
-    'dbcharset'=>'utf8'
-)
+    'dbconfig'=>array(
+        'dbhost'=>'127.0.0.1',
+        'dbuser'=>'root',
+        'dbpsw'=>'root',
+        'dbname'=>'blog',
+        'dbcharset'=>'utf8'
+    )
 );
 ?>  
 
--- kako/function/function.php
+++ kako/function/function.php
@@ -1,17 +1,17 @@
 <?php
     function C($name,$method){
-        require_once('/Controller/'.$name.'Controller.class.php');
+        require_once('Controller/'.$name.'Controller.class.php');
         eval('$obj = new '.$name.'Controller();$obj->'.$method.'();');
     }
 
     function M($name){
-        require_once('/Model/'.$name.'Model.class.php');
+        require_once('Model/'.$name.'Model.class.php');
         eval('$obj = new '.$name.'Model();');
         return $obj;
     }
 
     function V($name){
-        require_once('/View/'.$name.'View.class.php');
+        require_once('View/'.$name.'View.class.php');
         eval('$obj = new '.$name.'View();');
         return $obj;
     }
