96ff82c4ff74ce638380dba959476d62fa6a8e5e
xuandi.liu <xuandi.liu@gumichina.com>
2015-04-16 16:35:20 +0800

    修改一个手误，写错了变量名

--- Model/typeModel.class.php
+++ Model/typeModel.class.php
@@ -1,6 +1,6 @@
 <?php
     class typeModel{
-        public $table='type';
+        public $_table='type';
 
         function findAll_type($username){
             $sql='select * from '.$this->_table.' order by id desc';
